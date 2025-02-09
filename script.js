091564ded3c52f05e2adb3c42289c8fd41c2e6dd
Shekhar3742 <imderek3742@gmail.com>
2025-02-09 22:42:57 +0530

    deletepart

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+let ctr = 1;
+function deletetodo(index){
+     const element = document.getElementById(index);
+     element.parentNode.removeChild(element);
+}
+
 function addtodo() {
     const inputEl = document.querySelector("input");
     const value = inputEl.value;
 
     const newdivEL = document.createElement("div");
-    newdivEL.innerHTML = value;
-
+    newdivEL.setAttribute("id", ctr);
+   
+    newdivEL.innerHTML = "<div>" + value + "</div><button onclick='deletetodo(" + ctr + ")'>delete</button>";
+    ctr = ctr + 1 ;
     document.querySelector("body").appendChild(newdivEL)
     
 }
